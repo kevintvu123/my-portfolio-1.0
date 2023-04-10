335a32b826637134674d5ec03219a3abebb0931c
Kevin Vu <kevintvu123@gmail.com>
2023-04-10 19:23:52 -0400

    minor bug

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -16,9 +16,9 @@ function Contact() {
                 hidden: { opacity: 0, y: 0 },
             }}
         >
-            <h2 className='contact-title'>What's Next?</h2>
+            <h2 className='contact-title'>What&apos;s Next?</h2>
             <h2 className="contact-sub-title">Get in Touch</h2>
-            <p className="contact-text">My inbox is always open. Whether you have an opportunity, question, or just want to say hi. I'll get back to you as soon as possible. Thank you!</p>
+            <p className="contact-text">My inbox is always open. Whether you have an opportunity, question, or just want to say hi. I&apos;ll get back to you as soon as possible. Thank you!</p>
             <div className="contact-cta">
                 <Button text="Say Hello" link="mailto:kevintvu123@gmail.com" />
             </div>
