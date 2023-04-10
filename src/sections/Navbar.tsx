0999ae59a601296c55bdd4421286c49a2c907873
Kevin Vu <kevintvu123@gmail.com>
2023-04-10 18:16:12 -0400

    responsive navbar completed

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,7 +1,9 @@
-import Link from "next/link"
 import React, { useEffect, useState } from "react";
+import Link from "next/link"
 import Logo from "../components/Logo"
 import Button from "@/components/Button";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { CgClose } from "react-icons/cg";
 
 function Navbar() {
   const sectionLinks = [
@@ -14,12 +16,37 @@ function Navbar() {
   ];
 
   const [navbarVisible, setNavbarVisible] = useState(false)
+  const [responsiveNavVisible, setResponsiveNavVisible] = useState(false)
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
-      window.pageYOffset>100 ? setNavbarVisible(true) : setNavbarVisible(false);
+      window.pageYOffset > 100 ? setNavbarVisible(true) : setNavbarVisible(false);
     })
   }, [])
+
+  useEffect(() => {
+    const links = document.querySelectorAll(".nav-items-list-item-link");
+    links.forEach((link) => {
+      link.addEventListener("click", () => setResponsiveNavVisible(false));
+    });
+    const nav = document.querySelector(".nav-items");
+    nav?.addEventListener("click", (e) => {
+      e.stopPropagation();
+    });
+    const html = document.querySelector("html");
+    html?.addEventListener("click", (e) => {
+      setResponsiveNavVisible(false);
+    });
+  }, []);
+
+  useEffect(() => {
+    const main = document.querySelector("main");
+    if (responsiveNavVisible) {
+      main?.classList.add("blur");
+    } else {
+      main?.classList.remove("blur");
+    }
+  }, [responsiveNavVisible]);
 
   return (
     <nav>
@@ -29,7 +56,26 @@ function Navbar() {
             <Logo />
           </Link>
         </div>
-        <div className="nav-items">
+        <div className="nav-responsive-toggle">
+          {responsiveNavVisible ? (
+            <CgClose
+              onClick={(e) => {
+                e.stopPropagation();
+                setResponsiveNavVisible(false);
+              }}
+            />
+          ) : (
+            <GiHamburgerMenu
+              onClick={(e) => {
+                e.stopPropagation();
+                setResponsiveNavVisible(true);
+              }}
+            />
+          )}
+        </div>
+        <div
+          className={`${responsiveNavVisible && "nav-responsive"} nav-items`}
+        >
           <ul className="nav-items-list">
             {
               sectionLinks.map(({ name, link }) => (
@@ -42,7 +88,7 @@ function Navbar() {
             }
           </ul>
           <div className="nav-items-button">
-            <Button text="Resume" link="http://localhost:3000/resume.pdf"/>
+            <Button text="Resume" link="http://localhost:3000/resume.pdf" />
           </div>
         </div>
       </div>
